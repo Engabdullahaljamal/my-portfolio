243370616d37d3f061fd9b557a96e3dbeaa2b298
abdullah <en>
2025-07-24 15:18:22 -0400

    new update

--- src/component/Contact/Contact.jsx
+++ src/component/Contact/Contact.jsx
@@ -25,7 +25,7 @@ function Contact({ theme }) {
         {
             icon: <IoLocationOutline />,
             title: 'Address',
-            value: 'Totowa,New Jersey,USA',
+            value: 'Hackensack,New Jersey,USA',
         },
 
     ]
